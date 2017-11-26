04914ee4ee45f717b6ff35a542bf98c38ba87808
dh9325 <dariusz.huk@gmail.com>
2017-11-26 08:23:35 +0000

    Site redesign
    - add gulp tasks
    - required refactoring
    - remove redundant / add require dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,38 @@ var plumber = require('gulp-plumber');
 var less = require('gulp-less');
 var LessAutoprefix = require('less-plugin-autoprefix');
 var autoprefix = new LessAutoprefix({browsers: ['last 2 versions']});
-var livereload = require('livereload');
+var uglify = require('gulp-uglify');
+var cleanCss = require('gulp-clean-css');
 
-gulp.task('build', function () {
+gulp.task('build', ['js', 'css'], function () {
 
 });
 
 gulp.task('watch-less', function () {
-    return gulp.src('less/main.less')
-        .pipe(watch('less/main.less'))
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(gulp.dest('public/css'));
+  return gulp.src('less/main.less').
+    pipe(watch('less/main.less')).
+    pipe(plumber()).
+    pipe(less()).
+    pipe(gulp.dest('public/css'));
 });
 
-gulp.task('reload-less', ['watch-less', 'reload'], function () {
-
+gulp.task('js', function () {
+  return gulp.src('js/*.js').
+    pipe(sourcemaps.init()).
+    pipe(concat('main.js')).
+    pipe(uglify()).
+    pipe(sourcemaps.write()).
+    pipe(gulp.dest('public/js'));
 });
 
-gulp.task('reload', function () {
-    livereload.createServer().watch(__dirname + "/public");
+gulp.task('css', function () {
+  return gulp.src('less/main.less').
+    pipe(sourcemaps.init()).
+    pipe(plumber()).
+    pipe(less({
+      plugins: [autoprefix]
+    })).
+    pipe(cleanCss()).
+    pipe(sourcemaps.write()).
+    pipe(gulp.dest('public/css'));
 });
